410689eba7869c23b452410fbbd54121022b7d51
Marcin Wierciak <marcin.f.w@gmail.com>
2018-01-11 21:09:52 +0100

    fix errors in models

--- models/AdminModel.java
+++ models/AdminModel.java
@@ -1,66 +1,71 @@
 package models;
+import view.*;
 
 import java.security.acl.Group;
 import java.util.ArrayList;
 
 public class AdminModel extends UserModel {
 
-    public ArrayList<Group> groups = new ArrayList();
-    public ArrayList<MentorModel> mentors = new ArrayList();
+    private ArrayList<GroupModel> groups = new ArrayList();
+    private ArrayList<MentorModel> mentors = new ArrayList();
+    public AdminView view = new AdminView();
 
     public AdminModel(String login, String password, String name, String lastName){
         super(login, password, name, lastName);
         email = createEmail();
     }
 
-    public createMentor(String login, String password, String name, String lastName){
-        MentorModel newMentor = new MentorModel(login, password, name, lastName);
-        mentors.add(newMentor);
-        AdminView.displayText("Mentor created successfully");
-    }
+    // public void createMentor(String login, String password, String name, String lastName){
+    //     MentorModel newMentor = new MentorModel(login, password, name, lastName);
+    //     mentors.add(newMentor);
+    //     AdminView.displayText("Mentor created successfully");
+    // }
 
-    public createGroup(String name){
+    public void createGroup(String name){
         GroupModel newGroup = new GroupModel(name);
         groups.add(newGroup);
     }
 
-    public getGroups(){
+    public ArrayList<GroupModel> getGroups(){
         return groups;
     }
 
-    public getGroup(){
+    public GroupModel getGroup(){
         boolean groupNotChosen = true;
+        Integer groupIndex = 0;
         while(groupNotChosen){
-            AdminView.displayGroups(getGroups());
-            int groupIndex = (Integer) AdminView.getInput("Choose group number");
-            if (groupIndex < groups.size()){
-                groupNotChosen = true;
-                return groups.get(groupIndex);
+            view.displayGroups(getGroups());
+            String userInput = view.getInput("Choose group number");
+            groupIndex = Integer.parseInt(userInput);
+            if (groupIndex < groups.size()) {
+                groupNotChosen = false;
             }
         }
+        return groups.get(groupIndex);
     }
 
-    public getMentors(){
+    public ArrayList<MentorModel> getMentors(){
         return mentors;
     }
 
-    public getMentor(){
+    public MentorModel getMentor(){
         boolean mentorNotChosen = true;
+        String mentorIndex;
         while(mentorNotChosen){
-            AdminView.displayMentors(getMentors());
-            int mentorIndex = (Integer) AdminView.getInput("Choose mentor number");
-            if (mentorIndex < mentors.size()){
-                mentorNotChosen = true;
-                return mentors.get(mentorIndex);
+            view.displayMentors(getMentors());
+            mentorIndex = view.getInput("Choose mentor number");
+            if (mentorIndex.equals(mentors.size())){
+                mentorNotChosen = false;
             }
         }
+        return mentors.get(Integer.valueOf(mentorIndex));
     }
 
-    public assignMentorToGroup(){
+    public void assignMentorToGroup(){
         GroupModel groupToAssign = getGroup();
         MentorModel mentorToAssign = getMentor();
         groupToAssign.addMentor(mentorToAssign);
-        AdminView.displayText("Mentor assigned succesfully");
+        view.displayText("Mentor assigned succesfully");
     }
 
 }
--- models/MentorModel.java
+++ models/MentorModel.java
@@ -4,37 +4,35 @@ import java.util.ArrayList;
 
 public class MentorModel extends UserModel{
 
-    public ArrayList<Group> guidedGroups = new ArrayList();
+    public ArrayList<GroupModel> guidedGroups = new ArrayList();
 
     public MentorModel(String login, String password, String name, String lastName){
         super(login, password, name, lastName);
     }
 
-    public createStudent(String login, String password, String name, String lastName){
+    public void createStudent(String login, String password, String name, String lastName){
         boolean studentNotAdded = true;
         while(studentNotAdded){
-            String groupName = MentorView.getInput("Please enter name of group to add student")
+            String groupName = MentorView.getInput("Please enter name of group to add student");
             for (Group group : guidedGroups){
-                if group.name == groupName{
+                if (group.name == groupName) {
                     group = group.name;
                     studentNotAdded = false;
-                }
-                else{
-                    MentorView.displayText("There is no group like this.");
+                } else {
+                  MentorView.displayText("There is no group like this.");
                 }
             }
         StudentModel newStudent = new StudentModel(login, password, name, lastName, group);
         group.addStudent(newStudent);
         MentorView.displayText("Student added succesfully.");
     }
-
-    public getGroupByName(String name){
-        for (Group group : guidedGroups){
-            if(group.name == name){
-                return group;
-            }
-        }
-        return null;
-    }
-
+  }
 }
+    // public GroupModel getGroupByName(String name) {
+    //     for (GroupModel group : guidedGroups){
+    //         if(group.name == name){
+    //         }
+    //         return group;
+    //     }
+    //     return null;
+    // }
