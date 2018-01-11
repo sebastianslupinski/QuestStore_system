e8c3291c170d4d7bbedb661c6e16ce98c798f0fc
Marcin Wierciak <marcin.f.w@gmail.com>
2018-01-11 14:36:20 +0100

    add methods to MentorView class

---
+++ View/MentorView.java
@@ -0,0 +1,13 @@
+public class MentorView {
+  public void displayStudentWallet(String studentWallet) {
+    System.out.println("Wallet: " + studentWallet);
+  }
+
+  public void displayStudentData(String studentData) {
+    System.out.println("Student's info: " + studentData);
+  }
+
+  public void displayGroup(String groupToDisplay) {
+    System.out.println("Group: " + groupToDisplay);
+  }
+}
