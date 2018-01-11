8ebf3f1f23ce20f3c16965a651af0ab064bf1f32
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-01-11 18:11:13 +0100

    add hardcoded data for readDataFromFile method in MentorDAOImplement

--- dao/MentorDAOImplement.java
+++ dao/MentorDAOImplement.java
@@ -8,7 +8,11 @@ public class MentorDAOImplement implements MentorDAO {
 
     public List<MentorModel> mentorCollection = new ArrayList<MentorModel>();
 
-    public List<MentorModel> readDataFromFile() { return mentorCollection; }
+    public List<MentorModel> readDataFromFile() {
+        MentorModel newMentor = new MentorModel("mentor1", "password1", "Dominik", "S");
+        mentorCollection.add(newMentor);
+        return mentorCollection;
+    }
     public void saveDataToFile(List<MentorModel> mentorCollection) {}
     public void addObject(MentorModel object) {}
     public void deleteObject(MentorModel object) {}
