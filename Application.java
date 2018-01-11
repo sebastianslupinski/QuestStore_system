97116f1494674bc82115da849b22a971105590f8
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-01-11 14:56:59 +0100

    implement methods in Application.java and RootController.java to start application

---
+++ Application.java
@@ -0,0 +1,8 @@
+import Controller.*;
+
+public class Application{
+  public static void main(String[] args) {
+    RootController rootApplication = new RootController();
+    rootApplication.startApplication();
+  }
+}
