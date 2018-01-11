593c220a84ee0cfb32b661a0eb4bed14b8a59cb6
Marcin Wierciak <marcin.f.w@gmail.com>
2018-01-11 18:17:54 +0100

    add displayMenu method in AdminView class

--- view/AdminView.java
+++ view/AdminView.java
@@ -1,5 +1,20 @@
 public class AdminView extends ViewAbstract {
 
+  public void displayMenu(){
+
+   int optionNumber = 1;
+   System.out.println("======= HELLO-ADMIN =======");
+   System.out.println(" ");
+   System.out.println("Please choose option: ");
+   final String[] OPTIONS = {"Create Mentor", "Create Group", "Assign mentor to the group",
+                             "Show mentors profile", "Exit"};
+
+   for (String option : OPTIONS)
+   {
+       System.out.println(optionNumber + "--->" + option);
+       optionNumber ++;
+   }
+
   public void displayMentor(String mentorToDisplay) {
     System.out.println("Mentor: " + mentorToDisplay);
   }
