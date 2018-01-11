ca6cb1ee8a7cb13a9c804299d3d2f250b0d541f2
Marcin Wierciak <marcin.f.w@gmail.com>
2018-01-11 11:43:58 +0100

    add MentorController scheme

---
+++ Controller/MentorController.java
@@ -0,0 +1,40 @@
+public class MentorController {
+  private MentorModel model;
+  private MentorView view;
+
+  public MentorController(MentorModel model, MentorView view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  public createStudent() {
+  }
+
+  public changeGroup() {
+  }
+
+  public addNewQuest() {
+  }
+
+  public void addArtefactToStore() {
+  }
+
+  public void updateQuest() {
+  }
+
+  public void updateArtifact() {
+  }
+
+  public void splitArtifactToCategory() {
+  }
+
+  public void splitQuestToCategory() {
+  }
+
+  public void markStudentAchievedQuest() {
+  }
+
+  public void displayInfo() {
+  }
+
+}
