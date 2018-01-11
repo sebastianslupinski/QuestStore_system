0895418ed30cc835b6806bcbcafc3edee55bf64e
sebastianslupinski <bezerkey@gmail.com>
2018-01-11 10:17:25 +0100

    delete old files

--- Student.java
+++
@@ -1,9 +0,0 @@
-public class Student extends User{
-
-    String group;
-
-    public Student(String login, String password, String name, String lastName, String group){
-        super(login, password, name, lastName);
-        this.group = group;
-    }
-}
