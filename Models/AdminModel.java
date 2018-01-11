297cb3179a318e5be263307a363f0ed1a0099f28
sebastianslupinski <bezerkey@gmail.com>
2018-01-11 17:19:12 +0100

    add 'createMentor' method and mentors list in AdminModel class

--- Models/AdminModel.java
+++ Models/AdminModel.java
@@ -2,10 +2,17 @@ import java.util.ArrayList;
 
 public class AdminModel extends User{
 
-    //public ArrayList<Group> groups = new ArrayList();
+    public ArrayList<Group> groups = new ArrayList();
+    public ArrayList<MentorModel> mentors = new ArrayList();
 
     public AdminModel(String login, String password, String name, String lastName){
         super(login, password, name, lastName);
         email = createEmail();
+    }
+
+    public createMentor(String login, String password, String name, String lastName){
+        MentorModel newMentor = new MentorModel(login, password, name, lastName);
+        mentors.add(newMentor);
+        AdminView.displayText("Mentor created successfully");
     }
 }
