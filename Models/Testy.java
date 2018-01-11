b717f368be89646f53ff493988f2c6c1c78ded2e
sebastianslupinski <bezerkey@gmail.com>
2018-01-11 11:59:30 +0100

    add Quest class

--- Models/Testy.java
+++ Models/Testy.java
@@ -4,11 +4,15 @@ public class Testy{
         AdminModel admin = new AdminModel("admin1", "admin1", "name", "lastName");
         System.out.println(admin.toString());
         StudentModel student = new StudentModel("studenciak", "hasło", "name", "lastname", "A");
+        StudentModel student2 = new StudentModel("studenciak2", "hasło2", "name", "lastname", "A");
+        StudentModel student3 = new StudentModel("studenciak3", "hasło2", "name", "lastname", "A");
         System.out.println(student.toString());
         MentorModel mentor = new MentorModel("mentor","pass", "name", "lastname");
         System.out.println(student.group);
         System.out.println(mentor.toString());
         GroupModel grupa = new GroupModel("nowa grupa");
         grupa.addStudent(student);
+        grupa.addStudent(student2);
+        grupa.addStudent(student3);
     }
 }
