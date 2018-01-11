ef179f7d7bf71600fef9f92d41b873521140143a
Marcin Wierciak <marcin.f.w@gmail.com>
2018-01-11 11:45:06 +0100

    add StudentController scheme

---
+++ Controller/StudentController.java
@@ -0,0 +1,9 @@
+public class StudentController {
+  private StudentModel model;
+  private StudentView view;
+
+  public StudentController(StudentModel model, StudentView view) {
+    this.model = model;
+    this.view = view;
+  }
+}
