a7240cb6a1bd547376a66c5c3d91d37245a3f342
Marcin Wierciak <marcin.f.w@gmail.com>
2018-01-11 14:36:43 +0100

    add methods to StudentView class

--- View/StudentView.java
+++ View/StudentView.java
@@ -1,5 +1,14 @@
 public class StudentView {
-  public void displayWallet(String studentName, Object studentWallet){
-      System.out.println("Student's name: " + studentName);
-      System.out.println("Wallet: " + studentWallet);
+  public void displayWallet(String studentName, String studentWallet){
+    System.out.println("Student's name: " + studentName);
+    System.out.println("Wallet: " + studentWallet);
+  }
+
+  public void displayQuestBook(String questBook) {
+    System.out.println("QuestBook: " + questBook);
+  }
+
+  public void displayQuest(String questToDisplay) {
+    System.out.println("Quest: " + questToDisplay);
+  }
 }
