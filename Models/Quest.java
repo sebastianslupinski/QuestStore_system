9d8b56ed1f950f7c4066838fd48bdf743a7152ac
sebastianslupinski <bezerkey@gmail.com>
2018-01-11 13:19:16 +0100

    add setters and getters in Quest class

--- Models/Quest.java
+++ Models/Quest.java
@@ -1,4 +1,5 @@
 public abstract class Quest{
+
     public String name;
     public String description;
     public String category;
@@ -9,9 +10,35 @@ public abstract class Quest{
     public Quest(String name, String description, String category, int reward, boolean mark, String label){
         this.name = name;
         this.description = description;
-        this.category = category;
         this.reward = reward;
         this.mark = false;
         this.label = label;
+    }
+
+    public String getName(){ return name; }
+
+    public String getDescription(){ return description}
+
+    public int getReward(){ return reward }
+
+    public boolean getMark() { return mark }
+
+    public String getLabel() { return label}
+
+    public void setName(String newName) { this.name = newName; }
+
+    public void setDescription(String newDescription) { this.description = description;}
+
+    public void setReward(int newReward) { this.reward = newReward;}
+
+    public void setLabel(String newLabel) { this.label = newLabel;}
+
+    public changeMark(){
+        if (this.mark){
+            this.mark = false;
+        }
+        else {
+            this.mark = true;
+        }
     }
 }
