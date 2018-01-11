98b6f0c8c8cee748b44d3155c51cc75548089b77
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-01-11 16:25:42 +0100

    create dao folder and dao files inside

---
+++ dao/AdminDAOImplement.java
@@ -0,0 +1,14 @@
+package dao;
+
+import models.AdminModel;
+import java.util.List;
+
+public class AdminDAOImplement implements AdminDAO {
+
+    public static List<Admin> adminCollection = new List<Admin>;
+
+    public static List<Admin> readDataFromFile() { return new List<Admin>}
+    public static void saveDataToFile(List<Admin> adminCollection) {}
+    public static void addObject(Admin object) {}
+    public static void deleteObject(Admin object) {}
+}
