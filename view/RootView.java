2f68c38f5af6198f18ec505cb4c5f7184923a76c
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-01-11 17:28:10 +0100

    add methods to RootView

--- view/RootView.java
+++ view/RootView.java
@@ -1,11 +1,31 @@
 package view;
+import java.util.Scanner;
+
 
 public class RootView{
 
   public void displayText(String text){
     System.out.println(text);
   }
-  public void dispalyMenu(String menu){
 
+  public void displayMenu(){
+
+    int optionNumber = 1;
+    System.out.println("=======WELCOME IN QUESTSTORY SYSTEM=======");
+    System.out.println(" ")
+    System,out.println("Please choose your role: ")
+    final String[] OPTIONS = {"Admin", "Mentor", "Student"};
+
+    for (String option : OPTIONS)
+    {
+        System.out.println(optionNumber + "--->" + option);
+        optionNumber ++;
+    }
   }
+  public String getInput(){
+    Scanner scanner = new Scanner(system.in);
+    String input = scanner.nextLine();
+    return input;
+  }
+
 }
