10e44f7f04f4c451af9422b4426717e5daf7a6a3
Marcin Wierciak <marcin.f.w@gmail.com>
2018-01-11 15:56:38 +0100

    merge View methods with development

---
+++ View/AdminView.java
@@ -0,0 +1,10 @@
+public class AdminView extends ViewAbstract {
+
+  public void displayMentor(String mentorToDisplay) {
+    System.out.println("Mentor: " + mentorToDisplay);
+  }
+
+  public void displayGroups(String groupsToDisplay) {
+    System.out.println("Groups: " + groupsToDisplay);
+  }
+}
