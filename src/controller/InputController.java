3ab2994e4094ff4c8cd1ec54b84ec0c871f15a5f
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-01-17 16:46:03 +0100

    create method getString in InputController

--- src/controller/InputController.java
+++ src/controller/InputController.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class InputController{
-  
+
+  public String getString(){
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    return input;
+  }
 }
