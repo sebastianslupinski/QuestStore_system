0e18c07a2ede15a27e3a288766025d1faa51a56c
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-01-18 16:45:20 +0100

    change getPassword/Login methods in LoginController class

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -11,13 +11,11 @@ public class LoginController {
     InputController userInput = new InputController();
 
     public String getUserLogin() {
-        view.displayText("Please type your login: ");
-        return userInput.getString();
+        return userInput.getString("Please type your login: ");
     }
 
     public String getUserPassword() {
-        view.displayText("Please type your password: ");
-        return userInput.getString();
+        return userInput.getString("Please type your password: ");
     }
 
     public String[] validateLoginData(String login, String password) {
@@ -44,9 +42,11 @@ public class LoginController {
 
     public String[] processValidation() {
 
+        int idColumn = 0;
+        int roleColumn = 1;
         int counter = 3;
         int arrayCapacity = 2;
-        String[] idAndRole = {"wrong", "wrong"};
+        String[] idAndRole = new String[2];
         Boolean process = true;
 
         while (process && (counter > 0)) {
@@ -54,7 +54,7 @@ public class LoginController {
             String password = this.getUserPassword();
             idAndRole = this.validateLoginData(login, password);
             
-            if (idAndRole[0] == null || idAndRole[1] == null) {
+            if (idAndRole[idColumn] == null || idAndRole[roleColumn] == null) {
                 counter--;
                 view.displayText("Login or password incorrect, try again!");
             } else {
