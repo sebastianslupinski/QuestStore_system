fc62341f63697a1e58c3890485a91846ad1ed18c
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-01-18 15:24:47 +0100

    remove test printout from Application file

--- Application.java
+++ Application.java
@@ -1,9 +1,9 @@
 import src.controller.*;
 
-public class Application{
-  public static void main(String[] args) {
-    System.out.println("app");
-    RootController rootApplication = new RootController();
-    rootApplication.startApplication();
-  }
+public class Application {
+  
+    public static void main(String[] args) {
+        RootController rootApplication = new RootController();
+        rootApplication.startApplication();
+    }
 }
