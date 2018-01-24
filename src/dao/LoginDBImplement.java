506936a394040361a8cd0b61181e9bde77de5484
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-01-24 15:27:45 +0100

    refactor method createConnection in LoginDBImplement

--- src/dao/LoginDBImplement.java
+++ src/dao/LoginDBImplement.java
@@ -2,19 +2,46 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginDBImplement implements LoginDB {
 
-    public Connection createConnection() {
-        Connection connection = null;
+    Connection connection = null;
+    Statement statement = null;
+
+    public LoginDBImplement(){
+        this.connection=createConnection();
         try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:queststore.db");
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
+            this.statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        System.out.println("Connection with success");
-        return connection;
     }
+
+    public Connection createConnection() {
+
+        String url = "jdbc:sqlite:queststore.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("blleeeee");
+        return conn;
+
+    }
+
+    public void findUserIdAndRole(){
+//        String sql = "SELECT user_id, role\n" +
+//                "\tFROM logins\n" +
+//                "\tWHERE login='admin'\n" +
+//                "\tAND password='admin';";
+//        try (Connection connection = this.createConnection();
+//             Statement statement =
+//        )
+    }
+
+
 }
