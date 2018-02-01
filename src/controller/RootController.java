ac1c9cf52517143c8decb49563011d3b6647cd48
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-02-01 18:26:41 +0100

    implement method run in MentorController to display mentor menu

--- src/controller/RootController.java
+++ src/controller/RootController.java
@@ -5,10 +5,10 @@ import view.RootView;
 
 public class RootController{
 
-    RootView rootView = new RootView();
-    AdminController adminController = new AdminController();
-    // MentorController mentorController = new MentorController();
-    StudentController studentController = new StudentController();
+    private RootView rootView = new RootView();
+    private AdminController adminController = new AdminController();
+    private MentorController mentorController = new MentorController();
+//    StudentController studentController = new StudentController();
 
     public void startApplication(){
  
@@ -23,9 +23,9 @@ public class RootController{
             case "1":
                 adminController.run(id);
                 break;
-            // case "M":
-            //     mentorController.run(id);
-            //     break;
+             case "2":
+                 mentorController.run(id);
+                 break;
             // case "S":
             //     studentController.run(id);
             //     break;
