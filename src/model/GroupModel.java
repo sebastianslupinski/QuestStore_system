1ecdf943531531460e9026f66d58b1585810cd1e
sebastianslupinski <bezerkey@gmail.com>
2018-02-07 14:43:43 +0100

    create checker if mentor have group assigned to him already, one mentor can have one group now

--- src/model/GroupModel.java
+++ src/model/GroupModel.java
@@ -9,13 +9,17 @@ public class GroupModel{
     private String name;
     private int groupId;
     private String mentor_id;
-    ArrayList<StudentModel> students = new ArrayList<>();
+    private ArrayList<StudentModel> students;
 
     public GroupModel(String name, int groupId, String mentor_id, ArrayList students){
         this.name = name;
         this.groupId = groupId;
         this.mentor_id = mentor_id;
         this.students = students;
+    }
+
+    public ArrayList<StudentModel> getStudents() {
+        return students;
     }
 
 }
