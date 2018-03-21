b9f5ff6f6af4d5af3561f3709dce0b869322d621
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-03-21 14:21:01 +0100

    Add access modifiers to RootController class

--- src/main/java/controller/RootController.java
+++ src/main/java/controller/RootController.java
@@ -8,19 +8,18 @@ import java.sql.Connection;
 
 public class RootController{
 
-    RootView rootView = new RootView();
-    OpenCloseConnectionWithDB connectionWithDB = new OpenCloseConnectionWithDB();
-    Connection newConnection = connectionWithDB.getConnection();
-    LoginDBController loginController = new LoginDBController(newConnection);
+    private RootView rootView = new RootView();
+    private OpenCloseConnectionWithDB connectionWithDB = new OpenCloseConnectionWithDB();
+    private Connection newConnection = connectionWithDB.getConnection();
+    private LoginDBController loginController = new LoginDBController(newConnection);
 
     public void startApplication(){
  
         rootView.displayMenu();
         String[] idAndRole = loginController.processValidation();
-        String id = String.valueOf(idAndRole[0]).toString();
-        String role = String.valueOf(idAndRole[1]).toString();
+        String id = String.valueOf(idAndRole[0]);
 
-        String option = role;
+        String option = String.valueOf(idAndRole[1]);
         switch (option) {
             case "1":
                 AdminController adminController = new AdminController(newConnection);
