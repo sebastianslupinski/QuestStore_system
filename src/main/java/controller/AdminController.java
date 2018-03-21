1c57fb4a2c279bd50db046f34d10c91041693898
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-03-21 14:18:36 +0100

    Refactor connection with DB, delete connection as args moved to classes responsible for execute sql queries

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -40,7 +40,7 @@ public class AdminController {
     }
 
     public void run(String id) {
-        AdminModel admin = adminDB.loadAdmin(connection, Integer.valueOf(id));
+        AdminModel admin = adminDB.loadAdmin(Integer.valueOf(id));
         this.addExistingMentors(adminDB, admin);
         Integer option = 1;
 
@@ -52,30 +52,31 @@ public class AdminController {
                     view.displayListOfObjects(admin.getMentors());
                     break;
                 case 2:
-                    this.createMentor(admin, loginDB);
+                    MentorModel mentor = createMentor();
+                    addMentorToDb(mentor, admin);
                     break;
                 case 3:
-                    this.editMentor(admin, adminDB);
+                    editMentorProfile(admin, adminDB);
                     break;
                 case 4:
-                    this.assignMentorToGroup(admin, loginDB, adminDB, connection);
+                    assignMentorToGroup(admin);
                     break;
                 case 5:
-                    this.displayMentorsGroup(connection, admin, studentDB);
+                    displayMentorsGroup(admin, studentDB);
                     break;
                 case 6:
-                    this.editAdminInfo(admin);
-                    adminDB.exportAdmin(connection, admin);
+                    editAdminInfo(admin);
+                    adminDB.exportAdmin(admin);
                     break;
                 case 7:
                     connectionWithDB.closeConnection(connection);
-                    AdminView.displayText("Good bye");
+                    view.displayText("Good bye");
                     break;
             }
         }
     }
 
-    public void editAdminInfo(AdminModel admin){
+    private void editAdminInfo(AdminModel admin){
         boolean optionChosen = false;
         while(!optionChosen) {
             view.displayMenu(HEADER2, OPTIONS2);
@@ -128,7 +129,7 @@ public class AdminController {
         }
     }
 
-    public void addExistingMentors(AdminDB database, AdminModel admin) {
+    private void addExistingMentors(AdminDB database, AdminModel admin) {
         ArrayList<String[]> loginsInfo = database.getMentorsDataFromDatabase(2);
         for (String[] userInfo : loginsInfo) {
             MentorModel mentorToAdd;
@@ -143,25 +144,30 @@ public class AdminController {
         }
     }
 
-    public static MentorModel createMentor(AdminModel admin, LoginDB loginDB) {
-        String id = loginDB.getLastId();
+    private MentorModel createMentor() {
         String login = InputController.getString("Please enter mentor login: ");
         String password = InputController.getString("Please enter mentor password: ");
         String name = InputController.getString("Please enter mentor name: ");
         String lastName = InputController.getString("Please enter mentor lastName: ");
-        MentorModel newMentor = new MentorModel(id, login, password, name, lastName);
-        admin.getMentors().add(newMentor);
-        loginDB.saveNewUserToDatabase(newMentor);
-        MentorView.displayText("Mentor created successfully");
-        return newMentor;
+
+        return new MentorModel(login, password, name, lastName);
     }
 
-     public MentorModel getMentor(AdminModel admin){
+    private void addMentorToDb(MentorModel mentor, AdminModel admin) {
+        admin.getMentors().add(mentor);
+        loginDB.saveNewUserToDatabase(mentor);
+        view.displayText("Mentor created successfully");
+    }
+
+     private MentorModel getMentor(AdminModel admin){
        boolean mentorNotChosen = true;
        Integer mentorIndex = 0;
+
        while(mentorNotChosen){
+
            view.displayListOfObjects(admin.getMentors());
            mentorIndex = InputController.getNumber("Please enter a mentor number");
+
            if (mentorIndex < admin.getMentors().size()){
                mentorNotChosen = false;
            }
@@ -169,66 +175,75 @@ public class AdminController {
              view.displayText("Wrong number");
            }
        }
-       return admin.getMentors().get(Integer.valueOf(mentorIndex));
+       return admin.getMentors().get(mentorIndex);
      }
 
-     public void displayMentorsGroup(Connection connection, AdminModel admin, StudentDB studentDB){
+     private void displayMentorsGroup(AdminModel admin, StudentDB studentDB){
         view.displayText("Choose admin to show his class (press enter to continue)");
         InputController.getString();
+
         MentorModel mentorToShow = getMentor(admin);
         String mentorId = mentorToShow.getId();
-        GroupModel groupToShow = studentDB.getMentorGroupByMentorID(connection, mentorId);
+        GroupModel groupToShow = studentDB.getMentorGroupByMentorID(mentorId);
+
         view.displayText(mentorToShow.getName() + " " + mentorToShow.getLastName() + " Group:");
         view.displayListOfObjects(groupToShow.getStudents());
      }
 
-     public void editMentor(AdminModel admin, AdminDB database) {
+     private void editMentorProfile(AdminModel admin, AdminDB database) {
        boolean optionChosen = false;
+
        while(!optionChosen) {
-         MentorModel mentorToEdit = getMentor(admin);
-         String mentorId = mentorToEdit.getId();
-         view.displayMenu(HEADER2, OPTIONS2);
-         Integer option = InputController.getNumber("Enter your option");
-         switch (option) {
-           case 1:
-             String newLogin = InputController.getString("Enter new login");
-             mentorToEdit.setLogin(newLogin);
-             database.updateUserLogin(newLogin, mentorId);
-             optionChosen = true;
-             break;
-           case 2:
-             String newPassword = InputController.getString("Enter new password");
-             mentorToEdit.setPassword(newPassword);
-             database.updateUserPassword(newPassword, mentorId);
-             optionChosen = true;
-             break;
-           case 3:
-             String newName = InputController.getString("Enter new name");
-             mentorToEdit.setName(newName);
-             database.updateMentorsName(newName, mentorId);
-             optionChosen = true;
-             break;
-           case 4:
-             String newLastName = InputController.getString("Enter new lastname");
-             mentorToEdit.setLastName(newLastName);
-             database.updateMentorsLastName(newLastName, mentorId);
-             optionChosen = true;
-             break;
-           case 5:
-             String newEmail = InputController.getString("Enter new email");
-             mentorToEdit.setEmail(newEmail);
-             database.updateMentorsEmail(newEmail, mentorId);
-             optionChosen = true;
-             break;
+           MentorModel mentorToEdit = getMentor(admin);
+           String mentorId = mentorToEdit.getId();
+
+           view.displayMenu(HEADER2, OPTIONS2);
+           Integer option = InputController.getNumber("Enter your option");
+
+           switch (option) {
+               case 1:
+                 String newLogin = InputController.getString("Enter new login");
+                 mentorToEdit.setLogin(newLogin);
+                 database.updateUserLogin(newLogin, mentorId);
+                 optionChosen = true;
+                 break;
+
+               case 2:
+                 String newPassword = InputController.getString("Enter new password");
+                 mentorToEdit.setPassword(newPassword);
+                 database.updateUserPassword(newPassword, mentorId);
+                 optionChosen = true;
+                 break;
+
+               case 3:
+                 String newName = InputController.getString("Enter new name");
+                 mentorToEdit.setName(newName);
+                 database.updateMentorsName(newName, mentorId);
+                 optionChosen = true;
+                 break;
+
+               case 4:
+                 String newLastName = InputController.getString("Enter new last name");
+                 mentorToEdit.setLastName(newLastName);
+                 database.updateMentorsLastName(newLastName, mentorId);
+                 optionChosen = true;
+                 break;
+
+               case 5:
+                 String newEmail = InputController.getString("Enter new email");
+                 mentorToEdit.setEmail(newEmail);
+                 database.updateMentorsEmail(newEmail, mentorId);
+                 optionChosen = true;
+                 break;
          }
        }
      }
 
- public void assignMentorToGroup(AdminModel admin, LoginDB database,
-                                 AdminDB adminDatabase, Connection connection){
+ private void assignMentorToGroup(AdminModel admin){
         boolean mentorNotChosen = true;
         String mentorId = null;
-        ArrayList<String> mentorsIdsWithGroups = adminDatabase.getIdsOfMentorsHavingGroupsAlready(connection);
+        ArrayList<String> mentorsIdsWithGroups = adminDB.getIdsOfMentorsHavingGroupsAlready(connection);
+
         while (mentorNotChosen) {
             MentorModel mentorToAssign = getMentor(admin);
             mentorId = mentorToAssign.getId();
@@ -239,18 +254,18 @@ public class AdminController {
             else
                 {mentorNotChosen = false;}
         }
-           String newGroup = this.setGroupForMentor(database.getExistingGroups());
-           adminDatabase.createNewGroupAndAssignMentorToIt(newGroup, mentorId);
-           view.displayText("Mentor assigned succesfully");
+           String newGroup = this.setGroupForMentor(loginDB.getExistingGroups());
+           adminDB.createNewGroupAndAssignMentorToIt(newGroup, mentorId);
+           view.displayText("Mentor assigned successfully");
  }
 
-    public static String setGroupForMentor(Set<String> existingGroups) {
+    private String setGroupForMentor(Set<String> existingGroups) {
         String group = null;
         boolean groupNotChosen = true;
         while (groupNotChosen) {
             group = InputController.getString("Enter a group you want to create");
             if (existingGroups.contains(group)) {
-                AdminView.displayText("There is already group like this, create new group");
+                view.displayText("There is already group like this, create new group");
                 }
                 else {
                 groupNotChosen = false;
--- src/main/java/controller/MentorController.java
+++ src/main/java/controller/MentorController.java
@@ -40,11 +40,11 @@ public class MentorController {
         this.studentDB = new StudentDBImplement();
         // this.wallet = new WalletModel();
         this.mentorView = new MentorView();
-        this.existingStudents = studentDB.getAllStudents(connection);
+        this.existingStudents = studentDB.getAllStudents();
     }
 
     public void run(String id) {
-        GroupModel mentorGroup = studentDB.getMentorGroupByMentorID(connection, id);
+        GroupModel mentorGroup = studentDB.getMentorGroupByMentorID(id);
         Integer option = 1;
         while (!(option == 6)) {
             mentorView.displayMenu(HEADER, OPTIONS);
@@ -55,7 +55,7 @@ public class MentorController {
                     break;
                 case 2:
                     StudentModel studentToEdit = this.editStudent();
-                    studentDB.exportStudent(connection, studentToEdit);
+                    studentDB.exportStudent(studentToEdit);
                     break;
                 case 3:
                     mentorView.displayListOfObjects(mentorGroup.getStudents());
@@ -64,11 +64,11 @@ public class MentorController {
                     quest.questOption();
                     break;
                 case 5:
-                    MentorView.displayText("Artefacts option should be implemented here!");
+                    mentorView.displayText("Artefacts option should be implemented here!");
                     break;
                 case 6:
                     connectionWithDB.closeConnection(connection);
-                    MentorView.displayText("Good bye");
+                    mentorView.displayText("Good bye");
                     break;
             }
         }
@@ -78,21 +78,26 @@ public class MentorController {
     public void createStudent(Connection connection, StudentDB studentDB, GroupModel mentorGroup) {
         mentorView.displayText("This student will be added to your group, press enter to continue");
         InputController.getString();
+
         String id = loginDB.getLastId();
         String login = InputController.getString("Please enter student login: ");
         String password = InputController.getString("Please enter student password: ");
         String name = InputController.getString("Please enter student name: ");
         String lastName = InputController.getString("Please enter student lastName: ");
 //        String group = setGroupForMentor(getExistingGroups(admin));
+
         StudentModel newStudent = new StudentModel(id, login, password, name, lastName);
+
         loginDB.saveNewUserToDatabase(newStudent);
-        studentDB.insertNewStudentToGroup(connection, Integer.valueOf(newStudent.getId()), mentorGroup.getGroupId());
-        MentorView.displayText("Student created successfully");
+        studentDB.insertNewStudentToGroup(Integer.valueOf(newStudent.getId()), mentorGroup.getGroupId());
+
+        mentorView.displayText("Student created successfully");
     }
 
     public StudentModel editStudent() {
         boolean optionChosen = false;
         StudentModel studentToEdit = getStudent();
+
         while(!optionChosen) {
             mentorView.displayMenu(HEADER2, OPTIONS2);
             Integer option = InputController.getNumber("Enter your option");
